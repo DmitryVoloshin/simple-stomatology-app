09e67c2793998e7f27b0d94c23338f2c61633394
Dmitriy Voloshin <voloshincode@gmail.com>
2021-04-07 18:14:24 +0300

    added axios for request to api

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { useMediaPredicate } from "react-media-hook";
 
 
 
+
 //Components Imports
 import DesktopHeader from './Components/Page-components/Header/Desktop-header/DesktopHeader';
 import MobileHeader from './Components/Page-components/Header/Mobile-header/MobileHeader';
 import Main from './Components/Page-components/Main/Main';
+import Doctors from './Components/Page-components/Doctors/Doctors';
 
 
 
@@ -20,6 +22,7 @@ const App = () => {
     <div className="App">
         {isThisDesktop? <DesktopHeader/> : <MobileHeader/>}
         <Main/>
+        <Doctors/>
     </div>
   );
 }
